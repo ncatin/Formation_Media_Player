4a45942742c36f290e04359db6af9ab6ec3e18e8
ncatin <34193416+ncatin@users.noreply.github.com>
2019-10-20 15:07:28 -0400

    Update DancerDot.h

--- DancerDot.h
+++ DancerDot.h
@@ -2,22 +2,33 @@
 #include <vector>
 #include "Transition.h"
 #include <wx/gdicmn.h>
+#include <wx/dragimag.h>
 
 class DancerDot{
 public:
     DancerDot(wxString name, wxColour color){
       d_name = name;
       d_color = color;
-      d_bmp = new wxBitmap;
+      d_bmp = new wxBitmap(70, 70);
+      d_area = new wxRegion(*d_bmp);
+      d_drag = new wxDragImage(*d_bmp);
+  }
+
+  bool Contains(wxPoint pnt){
+      return d_area->Contains(pnt);
   }
 
   ~DancerDot(){
       delete d_bmp;
+      delete d_area;
+      delete d_drag;
   }
 private:
   wxString d_name;
   wxColour d_color;
   wxBitmap* d_bmp;
+  wxRegion* d_area;
+  wxDragImage* d_drag;
 
   std::vector<Transition> transition;
 
